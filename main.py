17802c0cd09a5b27c78214149ea12c541ada3902
xanxys <xanxys@gmail.com>
2012-11-24 23:30:30 +0900

    refactor Term

--- main.py
+++ main.py
@@ -14,6 +14,41 @@ import sys
 import math
 import msgpack
 
+class Term(object):
+	"""
+	Term GUI element
+	"""
+	def __init__(self, e):
+		self.e = e
+
+		if type(e) is float or type(e) is int or type(e) is bool or type(e) is str:
+			self.s = str(e)
+		elif type(e) is dict:
+			self.s = '{%d}'%len(e)
+		elif type(e) is tuple:
+			self.s = '[%d]'%len(e)
+		else:
+			self.s = 'error'
+	
+	def get_width(self):
+		return max(50, 10+6*len(self.s))
+
+	def as_string(self):
+		return self.s
+
+	def is_compound(self):
+		return type(self.e) is dict or type(self.e) is tuple
+
+	def get_type(self):
+		return type(self.e).__name__
+
+class Segment(object):
+	pass
+
+class Column(object):
+	pass
+
+		
 class Base:
 	def __init__(self, model):
 		self.model = model
@@ -29,35 +64,25 @@ class Base:
 		self.window.add(self.darea)
 		self.window.show_all()
 
+		self.layout = None
+
 	def main(self):
 		gtk.main()
 
 	def on_delete(self, w, ev):
 		return False
 
-	def on_expose(self, w, ev):
-		ctx = w.window.cairo_create()
-		ctx.set_source_rgb(0.153,0.157,0.133) # 272822
-		ctx.paint()
-
+	def do_layout(self, w, h):
 		# format
 		levels = {}
-		def to_flat(e):
-			if type(e) is list or type(e) is tuple:
-				return '[%d]'%len(e)
-			elif type(e) is dict:
-				return '{%d}'%len(e)
-			else:
-				return e
-
 		def st(e, lv=0):
 			if type(e) is list or type(e) is tuple:
-				levels.setdefault(lv,[]).append(map(to_flat, e))
+				levels.setdefault(lv,[]).append(map(Term, e))
 				for x in e:
 					st(x, lv+1)
 			elif type(e) is dict:
 				es = list(x for pair in e.items() for x in pair)
-				levels.setdefault(lv,[]).append(map(to_flat, es))
+				levels.setdefault(lv,[]).append(map(Term, es))
 				for x in es:
 					st(x, lv+1)
 
@@ -66,11 +91,10 @@ class Base:
 		# annotate w/ pos
 		seg_margin = 5
 
-		_, h = w.window.get_size()
 		levels_new = {}
 		level_left = 0
 		for (i, segs) in levels.items():
-			level_width = max(50, 10+6*max(len(str(elem)) for seg in segs for elem in seg))
+			level_width = max(elem.get_width() for seg in segs for elem in seg)
 
 			seg_top = 0
 			segs_new = []
@@ -115,29 +139,44 @@ class Base:
 
 			level_left += level_width
 
-		# 
+		self.layout = levels_new
+
+	def on_expose(self, w, ev):
+		"""
+		msgpack view: foreground + background
+		background = links, show overall tree structure
+		foreground = columns / column = segments / segment = entries + scrollbar
+
+		currently, we're assuming window is wide enough to contain all columns (this is very often true).
+		"""
+		ctx = w.window.cairo_create()
+		ctx.set_source_rgb(0.153,0.157,0.133) # 272822
+		ctx.paint()
+
+		self.do_layout(*w.window.get_size())
 		ctx.translate(5,0)
 
+		seg_margin = 5
 
 		# draw links
-		for (i, level) in levels_new.items():
+		for (i, level) in self.layout.items():
 			try:
 				ls_ix = 0
 				for (j, seg) in enumerate(level['segments']):
 					i_link = 0
 					for (k, elem) in enumerate(seg['children']):
-						if type(elem['body']) is str and (elem['body'][0] == '[' or elem['body'][0] == '{'):
+						if elem['body'].is_compound():
 							if i_link%2 == 0:
 								ctx.set_source_rgb(0.2,0.2,0.2)
 							else:
 								ctx.set_source_rgb(0.3,0.3,0.3)
 							# CW
-							ctx.move_to(levels_new[i+1]['left']-30, elem['bottom'])
+							ctx.move_to(self.layout[i+1]['left']-30, elem['bottom'])
 							ctx.line_to(elem['left'], elem['bottom'])
 							ctx.line_to(elem['left'], elem['top'])
-							ctx.line_to(levels_new[i+1]['left']-30, elem['top'])
-							ctx.line_to(levels_new[i+1]['left'], levels_new[i+1]['segments'][ls_ix]['top']+seg_margin/2)
-							ctx.line_to(levels_new[i+1]['left'], levels_new[i+1]['segments'][ls_ix]['bottom']-seg_margin/2)
+							ctx.line_to(self.layout[i+1]['left']-30, elem['top'])
+							ctx.line_to(self.layout[i+1]['left'], self.layout[i+1]['segments'][ls_ix]['top']+seg_margin/2)
+							ctx.line_to(self.layout[i+1]['left'], self.layout[i+1]['segments'][ls_ix]['bottom']-seg_margin/2)
 							ctx.fill()
 							ls_ix += 1
 							i_link+=1
@@ -146,7 +185,15 @@ class Base:
 				traceback.print_exc()
 
 		# draw content
-		for (i, level) in levels_new.items():
+		color_scheme = {
+			'int': (0.702,0.806,0.999),
+			'float': (0.682,0.506,0.999),
+			'str': (0.902,0.859,0.455),
+			'dict': (0.973,0.973,0.949),
+			'tuple': (0.973,0.973,0.949)
+		}
+
+		for (i, level) in self.layout.items():
 			for (j, seg) in enumerate(level['segments']):
 				# side line shadow
 				ctx.set_line_width(6)
@@ -171,18 +218,11 @@ class Base:
 				ctx.stroke()
 
 				for (k, elem) in enumerate(seg['children']):
-					if type(elem['body']) is float:
-						ctx.set_source_rgb(0.682,0.506,0.999) # #AE81FF
-					elif type(elem['body']) is int:
-						ctx.set_source_rgb(0.702,0.806,0.999)
-					elif type(elem['body']) is str and elem['body'][0] not in '[{':
-						ctx.set_source_rgb(0.902,0.859,0.455) # #E6DB74
-					else:
-						ctx.set_source_rgb(0.973,0.973,0.949) # #F8F8F2
-
 					ctx.save()
 					ctx.translate(elem['pos'][0]+5,elem['pos'][1]+10)
-					ctx.text_path(str(elem['body']))
+					ctx.text_path(elem['body'].as_string())
+
+					ctx.set_source_rgb(*color_scheme.get(elem['body'].get_type(), (1,0,0)))
 					ctx.fill()
 					ctx.restore()
 
